cf5715110789ce2027ff32a609ae8e862564ce1b
btiici <olanrewajupelumi8@gmail.com>
2024-07-17 07:51:33 +0100

    third commit

--- djetlawyerchatbot/src/components/Side.jsx
+++ djetlawyerchatbot/src/components/Side.jsx
@@ -1,25 +1,42 @@
 import sb from '../images/Bot (1).png'
 import create from '../images/Create.png'
 import trash from '../images/Trash Can.png'
+import bot from '../images/Bot.png'
+
 
 export default function Side (){
     return(
-        <div className='side'>
-            <div className='bar'>
-            <img src={sb}/>
-            <p>New Chat</p>
-            <img src={create} className='trash'/>
+        <div className='main'>
+            <div className='side'>
+                <div className='bar'>
+                    <img src={sb}/>
+                    <p>New Chat</p>
+                    <img src={create} className='trash'/>
+                </div>
+                <div className='bar'>        
+                    <p>Tell me about legal law <br /> <small>Legal law is a set of rules that sjsas ds ds ds d</small></p>
+                    <img src={trash} className='trash'/>
+                </div>
+                <div className='bar'>
+                    <p>Advantages of being  a lawyer <br /> <small>Legal law is a set of rules that sjsas ds ds ds d</small></p>
+                    <img src={trash} className='trash'/>
+                </div>
             </div>
-            <div className='bar'>
-            <img src={sb}/>
-            <p>New Chat</p>
-            <img src={trash} className='trash'/>
-            </div>
-            <div className='bar'>
-            <img src={sb}/>
-            <p>New Chat</p>
-            <img src={trash} className='trash'/>
-            </div>
+            <div className='middle'>
+                <div className='main1'>
+                    <img src={bot}/>
+                    <h3>How may I help you today?</h3>
+                </div>
+                <div className='question'>
+                    <p>Tell me about legal law <br /> <small>Legal law is a set of rules that sjsas ds ds ds d</small></p>
+                    <p>Tell me about legal law <br /> <small>Legal law is a set of rules that sjsas ds ds ds d</small></p>
+                    <p>Tell me about legal law <br /> <small>Legal law is a set of rules that sjsas ds ds ds d</small></p>
+                    <p>Tell me about legal law <br /> <small>Legal law is a set of rules that sjsas ds ds ds d</small></p>
+                </div>
+                
+            </div>    
+            
         </div>
+       
     )
 }
