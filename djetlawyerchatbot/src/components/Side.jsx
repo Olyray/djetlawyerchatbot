8be10b8d363cc8b3cf64747b05b3169f43e9fa43
btiici <olanrewajupelumi8@gmail.com>
2024-07-17 08:11:08 +0100

    fourth commit

--- djetlawyerchatbot/src/components/Side.jsx
+++ djetlawyerchatbot/src/components/Side.jsx
@@ -2,6 +2,8 @@ import sb from '../images/Bot (1).png'
 import create from '../images/Create.png'
 import trash from '../images/Trash Can.png'
 import bot from '../images/Bot.png'
+import Send from '../images/Send Letter.png'
+import question from '../images/Ask Question.png'
 
 
 export default function Side (){
@@ -33,9 +35,12 @@ export default function Side (){
                     <p>Tell me about legal law <br /> <small>Legal law is a set of rules that sjsas ds ds ds d</small></p>
                     <p>Tell me about legal law <br /> <small>Legal law is a set of rules that sjsas ds ds ds d</small></p>
                 </div>
-                
+                <div className='ask'>
+                <p>Tell me about legal law <br /> <small>Legal law is a set of rules that sjsas ds ds ds d</small></p>
+                <img src={Send}/>
+                </div>
             </div>    
-            
+            <img src={question} className='qMark'/>
         </div>
        
     )
