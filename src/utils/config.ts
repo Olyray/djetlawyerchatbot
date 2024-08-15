d3ea127183720762c7f16deb9be00a0368d6b75c
Olamide <olire96@gmail.com>
2024-08-15 14:08:03 +0100

    troubleshooting the BASE API URL

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log(process.env.PRODUCTION);
+
 export const API_BASE_URL = process.env.PRODUCTION === 'True'
   ? 'https://chatbotbackend-d5335f7520bc.herokuapp.com'
   : 'http://127.0.0.1:8000';
